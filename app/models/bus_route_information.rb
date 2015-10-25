0987cbe98e6e6b9923b6bd76c378e80deb2302ba
yama@muteki <yama@muteki.com>
2015-10-26 01:02:19 +0900

    add bus_type_label

--- app/models/bus_route_information.rb
+++ app/models/bus_route_information.rb
@@ -1,4 +1,12 @@
 class BusRouteInformation < ActiveRecord::Base
   belongs_to :bus_stop
   enum bus_type: { private_bus: 1, public_bus: 2, community_bus: 3, demand_bus: 4, other: 5 }
+
+  def bus_type_label
+    return "路線バス（民間）" if private_bus?
+    return "路線バス（公営）" if public_bus?
+    return "コミュニティバス" if community_bus?
+    return "デマンドバス" if demand_bus?
+    return "その他" if other?
+  end
 end
