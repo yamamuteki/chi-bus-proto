1ec37a0f5b2d2f317d2121ce1fd5121c22dd1cdf
yama@muteki <yama@muteki.com>
2015-10-26 08:35:58 +0900

    fix bus_type_label

--- app/models/bus_route_information.rb
+++ app/models/bus_route_information.rb
@@ -2,11 +2,15 @@ class BusRouteInformation < ActiveRecord::Base
   belongs_to :bus_stop
   enum bus_type: { private_bus: 1, public_bus: 2, community_bus: 3, demand_bus: 4, other: 5 }
 
+  BUS_TYPE_LABELS = {
+    private_bus: "路線バス（民間）",
+    public_bus: "路線バス（公営）",
+    community_bus: "コミュニティバス",
+    demand_bus: "デマンドバス",
+    other: "その他"
+  }
+
   def bus_type_label
-    return "路線バス（民間）" if private_bus?
-    return "路線バス（公営）" if public_bus?
-    return "コミュニティバス" if community_bus?
-    return "デマンドバス" if demand_bus?
-    return "その他" if other?
+    BUS_TYPE_LABELS[bus_type.to_sym]
   end
 end
