368b73940bd608ca80d1b1e6061c6619814a19b8
yama@muteki <yama@muteki.com>
2015-10-26 09:08:01 +0900

    fix n+1

--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -1,6 +1,6 @@
 class BusStopsController < ApplicationController
   def index
-    @bus_stops = BusStop.where('name like ?', "%#{params[:name]}%").limit 100
+    @bus_stops = BusStop.where('name like ?', "%#{params[:name]}%").limit(100).includes(:bus_route_informations)
     respond_to do | format |
       format.html { render :layout => nil }
       format.json { render json: @bus_stops }
