c6b57e06031ecb56c1eeb172fea6a4ae72c80d87
yama@muteki <yama@muteki.com>
2015-10-29 00:00:39 +0900

    add nothing message

--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -1,6 +1,7 @@
 class BusStopsController < ApplicationController
   def index
-    @bus_stops = BusStop.where('name like ?', "%#{params[:name]}%").limit(100).includes(:bus_route_informations)
+    like = params[:name].blank? ? "" : "%#{params[:name]}%"
+    @bus_stops = BusStop.where('name like ?', like).limit(100).includes(:bus_route_informations)
     respond_to do | format |
       format.html { render :layout => nil }
       format.json { render json: @bus_stops }
