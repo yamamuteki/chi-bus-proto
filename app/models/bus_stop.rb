d6c4f1f2745dcb6cd5714b7287eacd0978ef6c4f
yama@muteki <yama@muteki.com>
2015-10-31 23:51:39 +0900

    Fix bus route informations order.

--- app/models/bus_stop.rb
+++ app/models/bus_stop.rb
@@ -1,3 +1,3 @@
 class BusStop < ActiveRecord::Base
-  has_and_belongs_to_many :bus_route_informations
+  has_and_belongs_to_many :bus_route_informations, -> { order("operation_company, line_name") }
 end
