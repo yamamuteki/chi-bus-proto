d8a70c0153ad171d349e559325d30ba057c3cbca
yama@muteki <yama@muteki.com>
2015-11-04 02:09:01 +0900

    Change to nokogiri from rexml.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require 'nokogiri'
 require 'rexml/document'
 require 'objspace'
 require 'pp'
 
 def load_bus_stops_and_bus_route_informations
-  doc_bus_stops = REXML::Document.new(open("db/P11-10_12-jgd-g.xml"))
+  doc = Nokogiri::XML(open("db/P11-10_12-jgd-g.xml"))
+  doc.remove_namespaces!
 
   point_hash = {}
 
-  doc_bus_stops.elements.each('ksj:Dataset/gml:Point') do | element |
-    point_id = element.attributes['gml:id']
-    point_hash[point_id] = element.elements['gml:pos'].text
+  doc.css('Point').each do |element|
+    point_id = element['id']
+    point_hash[point_id] = element.at('pos').text
     print "#{point_id}\r"
   end
   puts
 
-  doc_bus_stops.elements.each('ksj:Dataset/ksj:BusStop') do | element |
-    name = element.elements['ksj:busStopName'].text
-    gml_id = element.attributes['gml:id']
-    point_id = element.elements['ksj:position/@xlink:href'].value.remove '#'
+  doc.css('BusStop').each do |element|
+    name = element.at('busStopName').text
+    gml_id = element['id']
+    point_id = element.at('position')['href'].remove '#'
     pos = point_hash[point_id]
 
-    bus_stop = BusStop.create(name: name, gml_id: gml_id, latitude: pos.split[0], longitude: pos.split[1] )
+    bus_stop = BusStop.create(name: name, gml_id: gml_id, latitude: pos.split[0], longitude: pos.split[1])
     
-    element.elements.each('.//ksj:BusRouteInformation') do | info |
-      bus_type = info.elements['ksj:busType'].text.to_i
-      operation_company = info.elements['ksj:busOperationCompany'].text
-      line_name = info.elements['ksj:busLineName'].text
+    element.css('BusRouteInformation').each do |info|
+      bus_type = info.at('busType').text.to_i
+      operation_company = info.at('busOperationCompany').text
+      line_name = info.at('busLineName').text
 
-      BusRouteInformation.find_or_create_by(bus_type: bus_type, operation_company: operation_company, line_name: line_name).bus_stops << bus_stop
+      bus_route_information = BusRouteInformation.find_or_create_by(bus_type: bus_type, operation_company: operation_company, line_name: line_name)
+      bus_route_information.bus_stops << bus_stop
     end
     print "#{gml_id}\r"
   end
@@ -43,11 +46,12 @@ def load_bus_stops_and_bus_route_informations
 end
 
 def load_bus_route_tracks_and_bus_routes
-  doc_routes = REXML::Document.new(open("db/N07-11_12.xml"))
+  doc = Nokogiri::XML(open("db/N07-11_12.xml"))
+  doc.remove_namespaces!  
 
-  doc_routes.elements.each('ksj:Dataset/gml:Curve') do | element |
-    curve_id = element.attributes['gml:id']
-    point_list = element.elements['gml:segments/gml:LineStringSegment/gml:posList'].text.strip
+  doc.css('Curve').each do |element|
+    curve_id = element['id']
+    point_list = element.at('posList').text.strip
 
     coordinates = []
     point_list.lines do |line|
@@ -60,15 +64,16 @@ def load_bus_route_tracks_and_bus_routes
   end
   puts
 
-  doc_routes.elements.each('ksj:Dataset/ksj:BusRoute') do | element |
-    gml_id = element.attributes['gml:id']
-    bus_type = element.elements['ksj:bsc'].text.to_i
-    operation_company = element.elements['ksj:boc'].text
-    line_name = element.elements['ksj:bln'].text
-    weekday_rate = element.elements['ksj:rpd'].text
-    saturday_rate = element.elements['ksj:rps'].text
-    holiday_rate = element.elements['ksj:rph'].text
-    note = element.elements['ksj:rmk'].text
+  doc.css('BusRoute').each do |element|
+    gml_id = element['id']
+    curve_id = element.at('brt')['href'].remove '#'
+    bus_type = element.at('bsc').text.to_i
+    operation_company = element.at('boc').text
+    line_name = element.at('bln').text
+    weekday_rate = element.at('rpd').text
+    saturday_rate = element.at('rps').text
+    holiday_rate = element.at('rph').text
+    note = element.at('rmk').text
 
     bus_route = BusRoute.find_or_create_by(
       bus_type: bus_type,
@@ -82,8 +87,6 @@ def load_bus_route_tracks_and_bus_routes
 
     bus_route_information = BusRouteInformation.find_by(bus_type: bus_type, operation_company: operation_company, line_name: line_name)
     bus_route_information.bus_route = bus_route if bus_route_information
-
-    curve_id = element.elements['ksj:brt/@xlink:href'].value.remove '#'
 
     bus_route_track = BusRouteTrack.find_by(gml_id: curve_id)
     bus_route.bus_route_tracks << bus_route_track if bus_route_track
